a412b520367da486767b6b16f6b0285585c1f957
Marcode777 <marcosebello7@gmail.com>
2016-09-22 06:10:02 -0400

    added node modules

---
+++ src/js/components/Layout.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {Link} from "react-router";
+
+
+export default class Layout extends React.Component{
+
+  render(){
+    return(
+      <div>
+        <h1> Hello World, this is Marco!</h1>
+      </div>
+      )
+  }
+}
