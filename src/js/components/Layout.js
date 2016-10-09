8d97150bb78b47df4d4f6ee863a6f61b34a8b58e
Marcode777 <marcosebello7@gmail.com>
2016-10-09 13:01:59 -0400

    modified src/index.html, src/js/components/Layout/js, src/js/components/Nav.js

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -2,14 +2,15 @@ import React from "react";
 import {Link} from "react-router";
 
 
+
 export default class Layout extends React.Component{
 
   render(){
     return(
       <div>
-        <h1> Hello World, this is Marco and welcome to my portfolio page!</h1>
-        
+          <h1> Hello World, this is Marco and welcome to my portfolio page!</h1>
       </div>
       )
   }
 }
+// have to figure out how to import Nav after creating Nav.js file in the same directory
